360a38f6821ea4d2b17d6a5d87c91187f35a93e8
double <2637309949@qq.com>
2019-06-17 15:50:59 +0800

    style: mit

--- dist/application.js
+++ dist/application.js
@@ -4,13 +4,9 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-/**
- * @author [Double]
- * @email [2637309949@qq.com]
- * @create date 2019-01-09 16:55:19
- * @modify date 2019-01-09 16:55:19
- * @desc [upload]
- */
+// Copyright (c) 2018-2020 Double.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
 const assert = require('assert').strict;
 
 const utils = require('./utils');
